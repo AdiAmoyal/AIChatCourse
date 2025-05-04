1aa80d8a9e62462635c66aeadbe582362a15e5b0
AdiAmoyal <adi.amoyal02@gmail.com>
2025-05-04 15:49:20 +0300

    Initial Commit

---
+++ AIChatCourse/AIChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseApp.swift
+//  AIChatCourse
+//
+//  Created by Adi Amoyal on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
