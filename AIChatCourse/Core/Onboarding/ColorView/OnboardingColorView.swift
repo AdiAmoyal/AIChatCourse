78411645926ca595d9511601f05a0d55c238141b
AdiAmoyal <adi.amoyal02@gmail.com>
2025-05-06 12:08:52 +0300

    [Feature] Add OnboardingIntroView, OnboardingColorView

---
+++ AIChatCourse/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingColorView.swift
+//  AIChatCourse
+//
+//  Created by Adi Amoyal on 06/05/2025.
+//
+
+import SwiftUI
+
+struct OnboardingColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingColorView()
+}
---
+++ AIChatCourse/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingIntroView.swift
+//  AIChatCourse
+//
+//  Created by Adi Amoyal on 06/05/2025.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingIntroView()
+}
