1f2cb1482b189431d4eb4217749485e2b5314371
AdiAmoyal <adi.amoyal02@gmail.com>
2025-05-06 11:39:10 +0300

    [Feature] Add WelcomeView UI

---
+++ AIChatCourse/Components/Images/ImageLoaderView.swift
@@ -0,0 +1,33 @@
+//
+//  ImageLoaderView.swift
+//  AIChatCourse
+//
+//  Created by Adi Amoyal on 06/05/2025.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ImageLoaderView: View {
+    
+    var urlString: String = Constants.randonImage
+    var resizingMode: ContentMode = .fill
+    
+    var body: some View {
+        Rectangle()
+            .opacity(0)
+            .overlay {
+                WebImage(url: URL(string: urlString))
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: resizingMode)
+                    .allowsHitTesting(false)
+            }
+            .clipped()
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+        .frame(width: 150, height: 300)
+}
---
+++ AIChatCourse/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  AIChatCourse
+//
+//  Created by Adi Amoyal on 06/05/2025.
+//
+
+import Foundation
+
+struct Constants {
+    static let randonImage = "https://picsum.photos/600/600"
+    static let privacyPolicyUrl = "https://www.apple.com"
+    static let termsOfServiceUrl = "https://www.apple.com"
+}
