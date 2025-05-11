8bd5639bf80c3ea00f3f49b6cd2e169b6188ee75
AdiAmoyal <adi.amoyal02@gmail.com>
2025-05-11 11:12:00 +0300

    [Feature] Sign in anonymously

--- AIChatCourse/Core/AppView/AppView.swift
+++ AIChatCourse/Core/AppView/AppView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 
 struct AppView: View {
     
+    @Environment(\.authService) private var authService
     @State var appState: AppState = AppState()
     
     var body: some View {
@@ -25,6 +26,26 @@ struct AppView: View {
             }
         )
         .environment(appState)
+        .task {
+            await checkUserStatus()
+        }
+    }
+    
+    private func checkUserStatus() async {
+        if let user = authService.getAuthenticatedUser() {
+            // User is authenticated
+            print("User already authenticated: \(user.uid)")
+        } else {
+            // User is not authenticated
+            do {
+                let result = try await authService.sighInAnonymously()
+                
+                // log in to app
+                print("Sign in anonympus success: \(result.user.uid)")
+            } catch let error {
+                print(error)
+            }
+        }
     }
 }
 
